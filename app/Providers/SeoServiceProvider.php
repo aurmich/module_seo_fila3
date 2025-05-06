338a5418c2fd93726c79f2e96ed64974b9a6aae1
marco76tv <marco.sottana@gmail.com>
2025-05-06 17:23:17 +0200

    .

--- app/Providers/SeoServiceProvider.php
+++ app/Providers/SeoServiceProvider.php
@@ -4,77 +4,29 @@ declare(strict_types=1);
 
 namespace Modules\Seo\Providers;
 
-<<<<<<< HEAD
-use Illuminate\Support\ServiceProvider;
+use Modules\Xot\Providers\XotBaseServiceProvider;
 use Modules\Seo\Services\MetatagService;
 
-class SeoServiceProvider extends ServiceProvider
+class SeoServiceProvider extends XotBaseServiceProvider
 {
-    /**
-     * @var string
-     */
-    protected string $moduleName = 'Seo';
+    public string $name = 'Seo';
 
-    /**
-     * @var string
-     */
-    protected string $moduleNameLower = 'seo';
+    protected string $module_dir = __DIR__;
 
-    /**
-     * Boot the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerConfig();
-        $this->registerViews();
-        $this->loadMigrationsFrom(module_path($this->moduleName, 'database/migrations'));
-    }
+    protected string $module_ns = __NAMESPACE__;
 
     /**
      * Register the service provider.
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
+        parent::register();
+
         $this->app->singleton(MetatagService::class, function ($app) {
             return new MetatagService();
         });
-    }
-
-    /**
-     * Register config.
-     *
-     * @return void
-     */
-    protected function registerConfig()
-    {
-        $this->publishes([
-            module_path($this->moduleName, 'config/config.php') => config_path($this->moduleNameLower . '.php'),
-        ], 'config');
-        $this->mergeConfigFrom(
-            module_path($this->moduleName, 'config/config.php'), $this->moduleNameLower
-        );
-    }
-
-    /**
-     * Register views.
-     *
-     * @return void
-     */
-    protected function registerViews()
-    {
-        $viewPath = resource_path('views/modules/' . $this->moduleNameLower);
-
-        $sourcePath = module_path($this->moduleName, 'resources/views');
-
-        $this->publishes([
-            $sourcePath => $viewPath
-        ], ['views', $this->moduleNameLower . '-module-views']);
-
-        $this->loadViewsFrom($sourcePath, $this->moduleNameLower);
     }
 
     /**
@@ -88,15 +40,4 @@ class SeoServiceProvider extends ServiceProvider
             MetatagService::class,
         ];
     }
-=======
-use Modules\Xot\Providers\XotBaseServiceProvider;
-
-class SeoServiceProvider extends XotBaseServiceProvider
-{
-    public string $name = 'Seo';
-
-    protected string $module_dir = __DIR__;
-
-    protected string $module_ns = __NAMESPACE__;
->>>>>>> 2ff0df4 (.)
 }
